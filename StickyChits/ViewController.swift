d1189c1005b2e54ef8d03390596c3a0c207e0df2
Bala <baluatcomputerscience@gmail.com>
2020-11-24 22:05:57 +0530

    Initial Commit

---
+++ StickyChits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StickyChits
+//
+//  Created by Bala on 24/11/20.
+//  Copyright © 2020 Bala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
